84793b759ffccb11eaf222913067ff0be823e653
yashawasthi <47225006+yashawasthi@users.noreply.github.com>
2022-05-26 13:34:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import SignUp from './components/signup/SignUp';
+// import SignUp from './components/signup/SignUp';
 import HomePage from './components/Homepage/HomePage';
-import Login from './components/login/Login';
+// import Login from './components/login/Login';
 function App() {
   return (
     <div className='main_div'>
        <BrowserRouter>
       <Routes>
           <Route path="/" element={<HomePage />} /> 
-          <Route path="signup" element={<SignUp />} />
-          <Route path="login" element={<Login />} />
+          {/* <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} /> */}
           {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </BrowserRouter>
